d703555a7d6ffdddf33611af626b9d6684502444
emir-masinovic <emir3710549@gmail.com>
2023-11-20 11:46:53 +0300

    message test2

--- src/first_node/first_node.py
+++ src/first_node/first_node.py
@@ -8,13 +8,21 @@ def callback(data):
     concatenated_message = "First node: " + received_message
     rospy.loginfo("Received: %s", received_message)
     rospy.loginfo("Publishing: %s", concatenated_message)
-    pub = rospy.Publisher('first_node_topic', String, queue_size=10)
+    pub = rospy.Publisher('/first_node_topic', String, queue_size=10)
     pub.publish(concatenated_message)
 
-def listener():
+def hello_publisher():
     rospy.init_node('first_node', log_level=rospy.DEBUG, anonymous=True)
     rospy.Subscriber('original_topic', String, callback)
-    rospy.spin()
+    rate = rospy.Rate(1)  # 1 Hz, i.e., publish every 1 second
+
+    while not rospy.is_shutdown():
+        pub = rospy.Publisher('/first_node_topic', String, queue_size=10)
+        pub.publish("Hello world")
+        rate.sleep()
 
 if __name__ == '__main__':
-    listener()
+    try:
+        hello_publisher()
+    except rospy.ROSInterruptException:
+        pass
